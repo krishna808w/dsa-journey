e0f4aef72015a8f0df40e6f58006ce0b847062f4
krishna808w <akrishnavnr@gmail.com>
2026-09-09 13:33:08 +0530

    longest common prefix

--- 03-strings/01-longest-common-prefix.py
+++ 03-strings/01-longest-common-prefix.py
@@ -1,20 +1,24 @@
-strs = ["flower", "flow", "flight"]
+def find_longest_prefix(str_list):
+  longest_prefix=""
+  if(len(str_list)==0):
+    return longest_prefix
+  for j in range(len(str_list[0])):
+    for i in range(1,len(str_list)):
+      if j >= len(str_list[i]) or str_list[i][j] != str_list[0][j]:
+        return longest_prefix
+    longest_prefix+=str_list[0][j]
+  return longest_prefix
+  
+        
+       
+
+print("1",find_longest_prefix(["flower", "flow", "flight"]))
+print("2",find_longest_prefix(["dog", "racecar", "car"]))
+print("3",find_longest_prefix(["flower", "flower"]))
+print("4",find_longest_prefix([]))
+print("5",find_longest_prefix([""]))
 
 
-def longest_common_prefix(strs):
-    prefix = ""
-
-    for i in range(len(strs[0])):
-        for j in range(len(strs)):
-            if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
-                return prefix
-
-        prefix += strs[0][i]
-
-    return prefix
-
-
-print(longest_common_prefix(strs))
 
 # Time Complexity: O(n * m)
 # n = number of strings
