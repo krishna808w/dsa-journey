9b8d66fed48fc3aac9f612bd9217dc86536981c3
krishna808w <akrishnavnr@gmail.com>
2026-09-21 23:11:20 +0530

    feat: wrap two sum implementation in a function and add test cases

--- 01_arrays/08_two_sum.py
+++ 01_arrays/08_two_sum.py
@@ -1,8 +1,16 @@
-for i in range(len(num_array)):
+def two_sum(num_array, target):
+  seen={}
+  for i in range(len(num_array)):
     if target - num_array[i] in seen:
         return [i, seen[target - num_array[i]]]
 
     seen[num_array[i]] = i
+
+print(two_sum([3, 2, 4], 6))       # [1, 2]
+print(two_sum([3, 3], 6))      # [0, 1]
+print(two_sum([1, 2, 3], 10))      # None
+print(two_sum([-1, -2, -3, -4], -6))  # [1, 3]
+
 
 
 #Time: O(n) average
